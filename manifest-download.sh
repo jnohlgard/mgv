97129317ea1831120511c05be8bb08f41be136b5
Joakim Nohlgård <joakim@nohlgard.se>
2021-02-09 12:05:36 +0100

    manifest-download.sh: Download multiple files and verify them

---
+++ manifest-download.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+set -euo pipefail
+
+usage() {
+  printf 'Usage: %s <manifest-file> <destination-file1> [... <destination-fileN>]\n\n' "$0"
+  printf 'Download the given files and verify agains the manifest-file\n'
+}
+
+if [ "$#" -lt 2 ] || [ "$1" = '--help' ]; then
+  >&2 usage
+  exit 1
+fi
+
+manifest_file=$1;shift
+if [ ! -r "${manifest_file}" ]; then
+  >&2 printf 'Error: Not readable: %s\n' "${manifest_file}"
+fi
+
+tools_path=$(cd "$(dirname "$0")"; pwd)
+download_cmd="${tools_path}/download-src-uri.sh"
+verify_cmd="${tools_path}/verify-manifest.sh"
+
+ret=0
+for dest in "$@"; do
+  code=0
+  "${download_cmd}" "${dest}" || code=$?
+  if [ "${code}" -ne 0 ]; then
+    >&2 printf '%s: Download failure\n' "${dest}"
+    ret=${code}
+    continue
+  fi
+  "${verify_cmd}" "${manifest_file}" "${dest}" || code=$?
+  if [ "${code}" -ne 0 ]; then
+    mv -f "${dest}" "${dest}.verify-failed"
+    >&2 printf '%s: Verification failure\n' "${dest}"
+    ret=${code}
+    continue
+  fi
+done
+exit ${ret}
